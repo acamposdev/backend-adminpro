6a3b888c7fd547b13dec6e3d4f6a14364eb3cbce
acamposdev <alejandro.acg@outlook.com>
2018-08-18 20:18:20 +0200

    Correccion del middleware para CORS

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var app = express(); // Definicion del servidor express
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow_Methods", "POST", "PUT", "GET", "DELETE", "OPTIONS");
+    res.header("Access-Control-Allow-Methods", "POST, PUT, GET, DELETE, OPTIONS");
     next();
 });
 
